0ef57c07519a9b9b1da8d0bb5d788f4e2ae26f78
ayoub-cs <ijarayoub@gmail.com>
2025-10-28 15:14:35 +0000

    Initial commit - projet Spring Boot avec tests et Swagger

---
+++ src/main/java/com/example/studentmanagement/repository/StudentRepository.java
@@ -0,0 +1,17 @@
+package com.example.studentmanagement.repository;
+
+import com.example.studentmanagement.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    Student findById(int id);
+
+    @Query("SELECT YEAR(s.dateNaissance), COUNT(s) FROM Student s GROUP BY YEAR(s.dateNaissance)")
+    Collection<Object[]> findNbrStudentByYear();
+}
